c1bc8d10622b9e7b1b0ec908ce579e1efd339918
bekirbali <bfbali43@gmail.com>
2024-03-25 18:33:43 +0300

    models update

--- app/models/models.ts
+++ app/models/models.ts
@@ -4,28 +4,14 @@ export interface Urun {
   id: number;
 }
 
-export interface HaftalikRapor {
-  hafta: number;
-  toplam_satis: number;
-  urunler: Urun[];
-}
-
 export interface GunlukRapor {
   tarih: string;
   toplam_satis: number;
   urunler: Urun[];
 }
 
-export interface AylikRapor {
-  ay: string;
-  toplam_satis: number;
-  urunler: Urun[];
-}
-
 export interface DummyData {
-  haftalik_raporlar: HaftalikRapor[];
   gunluk_raporlar: GunlukRapor[];
-  aylik_raporlar: AylikRapor[];
 }
 
 export interface IDrawerProps {
--- app/utils/data.ts
+++ app/utils/data.ts
@@ -1,18 +1,6 @@
 import { DummyData } from "../models/models";
 
 export const dummyData: DummyData = {
-  haftalik_raporlar: [
-    {
-      hafta: 1,
-      toplam_satis: 1500,
-      urunler: [
-        { id: 1, urun_adi: "Ürün 1", adet: 20 },
-        { id: 2, urun_adi: "Ürün 2", adet: 30 },
-        { id: 3, urun_adi: "Ürün 3", adet: 15 },
-      ],
-    },
-    // Diğer haftalık raporlar...
-  ],
   gunluk_raporlar: [
     {
       tarih: "2024-02-24",
@@ -23,18 +11,5 @@ export const dummyData: DummyData = {
         { id: 6, urun_adi: "Ürün 3", adet: 3 },
       ],
     },
-    // Diğer günlük raporlar...
-  ],
-  aylik_raporlar: [
-    {
-      ay: "Şubat 2024",
-      toplam_satis: 6500,
-      urunler: [
-        { id: 7, urun_adi: "Ürün 1", adet: 100 },
-        { id: 8, urun_adi: "Ürün 2", adet: 150 },
-        { id: 9, urun_adi: "Ürün 3", adet: 80 },
-      ],
-    },
-    // Diğer aylık raporlar...
   ],
 };
